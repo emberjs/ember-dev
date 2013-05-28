3fb81a5083369dded728d7e3a90985d18919522e
Stefan Penner <stefan.penner@gmail.com>
2013-05-27 20:57:36 -0400

    improve readability of to_s3 script

--- lib/ember-dev/publish.rb
+++ lib/ember-dev/publish.rb
@@ -1,39 +1,61 @@
-module EmberDev
-  class Publish
-    require 'aws-sdk'
+require 'aws-sdk'
 
-    def self.to_s3(opts={})
-      access_key_id = opts.fetch :access_key_id
-      secret_access_key = opts.fetch :secret_access_key
-      bucket_name = opts.fetch :bucket_name
-      files = opts.fetch :files
+module EmberDev
+  module Publish
+    include self;
+
+    def to_s3(opts={})
+      files = opts.fetch(:files)
+      bucket_name = opts.fetch(:bucket_name)
+      access_key_id = opts.fetch(:access_key_id)
+      secret_access_key = opts.fetch(:secret_access_key)
+
       subdirectory = opts[:subdirectory] ? opts[:subdirectory] + '/' : ''
-      rev=`git rev-list HEAD -n 1`.to_s.strip
+
+      rev = `git rev-list HEAD -n 1`.to_s.strip
+
       master_rev = `git rev-list origin/master -n 1`.to_s.strip
+
       return unless rev == master_rev
       return unless access_key_id && secret_access_key && bucket_name
+
       s3 = AWS::S3.new(
-        :access_key_id => access_key_id,
-        :secret_access_key => secret_access_key)
+        access_key_id: access_key_id,
+        secret_access_key: secret_access_key)
+
       bucket = s3.buckets[bucket_name]
+
       files.each do |file|
         basename = Pathname.new(file).basename.sub_ext('')
+
         unminified_targets = [
           "#{basename}-latest.js",
           "#{basename}-#{rev}.js"
-        ].map { |file| bucket.objects[subdirectory + file] }
-        unminified_targets.each { |obj| obj.write(Pathname.new(file)) }
+        ].map do |file|
+          bucket.objects[subdirectory + file]
+        end
+
+        unminified_targets.each do |obj|
+          obj.write(Pathname.new(file))
+        end
+
         minified_source = file.sub(/#{basename}.js$/, "#{basename}.min.js")
+
         minified_targets = [
           "#{basename}-latest.min.js",
           "#{basename}-#{rev}.min.js"
-        ].map { |file| bucket.objects[subdirectory + file] }
+        ].map do |file|
+          bucket.objects[subdirectory + file]
+        end
+
         prod = bucket.objects["#{subdirectory}#{basename}.prod.js"]
         prod.write Pathname.new file.sub(/#{basename}.js$/, "#{basename}.prod.js")
-        minified_targets.each { |obj| obj.write(Pathname.new(minified_source)) }
+
+        minified_targets.each do |obj|
+          obj.write(Pathname.new(minified_source))
+        end
       end
     end
-
   end
 end
 

85334461e0ce98b28bd9cd6b1d5841177658b108
Martin Muñoz <im.mmun@gmail.com>
2015-08-24 15:03:18 -0400

    Refactor AssertionAssert to work with module debug functions

--- addon/test-helper/utils.js
+++ addon/test-helper/utils.js
@@ -20,3 +20,19 @@ export function buildCompositeAssert(assertClasses) {
 
   return Composite;
 }
+
+function noop() {}
+
+export function callWithStub(env, name, func, debugStub=noop) {
+  let originalFunc = env.getDebugFunction(name);
+  try {
+    env.setDebugFunction(name, debugStub);
+    func();
+  } finally {
+    env.setDebugFunction(name, originalFunc);
+  }
+}
+
+export function checkTest(test) {
+  return (typeof test === 'function') ? test() : test;
+}
--- tests/unit/assertion-test.js
+++ tests/unit/assertion-test.js
@@ -1,11 +1,22 @@
+import Ember from 'ember';
 import AssertionAssert from 'ember-dev/test-helper/assertion';
+import makeEnv from '../helpers/make-env';
 
-var originalOk = QUnit.ok;
-var assertion;
+let originalOk;
+let originalAssert;
+let assertion;
+
+let env = makeEnv();
 
 module('AssertionAssert', {
-  teardown: function(){
+  beforeEach() {
+    originalOk = QUnit.ok;
+    originalAssert = env.getDebugFunction('assert');
+  },
+  afterEach() {
     QUnit.ok = originalOk;
+    env.setDebugFunction('assert', originalAssert);
+
     if (assertion) {
       assertion.restore();
       assertion = null;
@@ -16,8 +27,7 @@ module('AssertionAssert', {
 test('expectAssertion fires when an expected assertion is not called', function(){
   expect(2);
 
-  var Ember = {};
-  assertion = new AssertionAssert({Ember: Ember});
+  assertion = new AssertionAssert(env);
 
   assertion.inject();
   window.expectAssertion(function(){
@@ -31,8 +41,7 @@ test('expectAssertion fires when an expected assertion is not called', function(
 test('expectAssertion fires when an expected assertion does not pass for boolean values', function(){
   expect(2);
 
-  var Ember = { assert: function(){} };
-  assertion = new AssertionAssert({Ember: Ember});
+  assertion = new AssertionAssert(env);
 
   assertion.inject();
   window.expectAssertion(function(){
@@ -44,8 +53,7 @@ test('expectAssertion fires when an expected assertion does not pass for boolean
 test('expectAssertion fires when an expected assertion does not pass for functions', function(){
   expect(2);
 
-  var Ember = { assert: function(){} };
-  assertion = new AssertionAssert({Ember: Ember});
+  assertion = new AssertionAssert(env);
 
   assertion.inject();
   window.expectAssertion(function(){
@@ -59,8 +67,7 @@ test('expectAssertion fires when an expected assertion does not pass for functio
 test('expectAssertion does not fire when an expected assertion passes for boolean values', function(){
   expect(2);
 
-  var Ember = { assert: function(){} };
-  assertion = new AssertionAssert({Ember: Ember});
+  assertion = new AssertionAssert(env);
 
   assertion.inject();
   window.expectAssertion(function(){
@@ -75,8 +82,7 @@ test('expectAssertion does not fire when an expected assertion passes for boolea
 test('expectAssertion does not fire when an expected assertion passes for functions', function(){
   expect(2);
 
-  var Ember = { assert: function(){} };
-  assertion = new AssertionAssert({Ember: Ember});
+  assertion = new AssertionAssert(env);
 
   assertion.inject();
   window.expectAssertion(function(){
@@ -93,8 +99,11 @@ test('expectAssertion does not fire when an expected assertion passes for functi
 test('ignoreAssertion silences assertions', function(){
   expect(1);
 
-  var Ember = { assert: function(){ originalOk(false, 'should not call assert'); } };
-  assertion = new AssertionAssert({Ember: Ember});
+  env.setDebugFunction('assert', function() {
+    originalOk(false, 'should not call assert');
+  });
+
+  assertion = new AssertionAssert(env);
 
   assertion.inject();
   window.ignoreAssertion(function(){
